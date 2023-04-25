ef4a7f44d582d0ea1132d78b8eb1fa44118760d6
Romans Špiļaks <romans.spilaks@gmail.com>
2023-04-25 22:56:04 +0300

    Add id for each book item. Add error messages for existing books and empty input fields. Add basic styles.

--- main.js
+++ main.js
@@ -1,47 +1,73 @@
-const formElement = document.getElementById("form");
-const bookList = document.getElementById("book_list");
-
-let bookListElements = JSON.parse(localStorage.getItem("books")) || [];
-
-/* eslint-disable no-use-before-define */
-
 class Book {
-  constructor(title, author) {
-    const title = document.getElementById("title_input");
-    const author = document.getElementById("author_input");
-    this.title = title;
-    this.author = author;
-  }
+  constructor() {
+    const storedBooks = localStorage.getItem('books');
+    this.books = storedBooks ? JSON.parse(storedBooks) : [];
+    this.lastId = (this.books.length > 0) ?
+      this.books[this.books.length - 1].id : 0;
+    }
 
-  addNewBook() {
-    const newBook = new Book(title.value, author.value);
-    bookListElements.push(newBook);
-    localStorage.setItem("books", JSON.stringify(bookListElements));
-    displayAllBooks();
-    title.value = "";
-    author.value = "";
-  }
-
-  removeBook(book) {
-    bookListElements = bookListElements.filter((element) => element !== book);
-    localStorage.setItem("books", JSON.stringify(bookListElements));
-    displayAllBooks();
-  }
-
-  displayAllBooks() {
-    bookList.innerHTML = "";
-    bookListElements.forEach((book) => {
-      const bookListItem = document.createElement("li");
-      const line = document.createElement("hr");
-      bookListItem.innerHTML = `${book.bookTitle}<br/>${book.bookAuthor} <br/>`;
-      const btnRemove = document.createElement("button");
-      btnRemove.innerHTML = "Remove";
-      btnRemove.addEventListener("click", () => removeBook(book));
-      bookList.appendChild(bookListItem);
-      bookList.appendChild(btnRemove);
-      bookList.appendChild(line);
-    });
+  addBook () {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    if(title.trim() === '' || author.trim() === '') {
+      const err = document.createElement('div');
+      err.classList.add('error');
+      err.textContent = "Please enter a title and an author";
+      setTimeout(() => err.remove(), 3500);
+      document.body.appendChild(err);
+     }
+      else if (this.books.some((book) => book.title === title)) {
+        const err = document.createElement('div');
+        err.classList.add('error');
+        err.textContent = "This book already exists";
+        setTimeout(() => err.remove(), 3500);
+        document.body.appendChild(err);
+      } else {
+    const book = {id: ++this.lastId, title, author};
+    this.books.push(book);
+    localStorage.setItem("books", JSON.stringify(this.books));
+    title = '';
+    author = '';
+    this.displayAllBooks();
   }
 }
 
-displayAllBooks();
+  removeBook(bookId) {
+    this.books = this.books.filter((book) => book.id !== bookId);
+    localStorage.setItem("books", JSON.stringify(this.books));
+  }
+
+  displayAllBooks() {
+    const bookList = document.getElementById('book_list');
+    bookList.innerHTML = "";
+    for (const book of this.books) {
+      const bookListItem = document.createElement("li");
+      bookListItem.innerHTML = `${book.title} <br /> ${book.author}`
+      bookListItem.setAttribute('id', book.id);
+      const line = document.createElement("hr");
+      const btnRemove = document.createElement("button");
+      btnRemove.classList.add('remove-btn')
+      btnRemove.textContent = "Remove";
+      btnRemove.addEventListener('click', () => {
+        this.removeBook(book.id);
+        this.displayAllBooks();
+      });
+      bookListItem.appendChild(btnRemove);
+      bookList.appendChild(bookListItem);
+      bookList.appendChild(line);
+    }
+  };
+}
+
+  const newBook = new Book();
+
+const addBtn = document.getElementById('submit_btn')
+const formElement = document.getElementById('form');
+
+newBook.displayAllBooks();
+
+addBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  newBook.addBook();
+  newBook.displayAllBooks();
+});
