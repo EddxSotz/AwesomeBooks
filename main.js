8cff40cff8ca1cd4437ed723e460cccaa194acc5
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2023-04-25 22:38:20 -0600

    Modify main.js and style changes

--- main.js
+++ main.js
@@ -1,73 +1,64 @@
+
+const form = document.getElementById('form');    
+const allBooks = document.getElementById('book_list');
+const messageField = document.getElementById('messageField');
+
+let bookList = JSON.parse(localStorage.getItem('BooksList')) || []; //global variable to store from localStorage
+
+//Book class
 class Book {
   constructor() {
-    const storedBooks = localStorage.getItem('books');
-    this.books = storedBooks ? JSON.parse(storedBooks) : [];
-    this.lastId = (this.books.length > 0) ?
-      this.books[this.books.length - 1].id : 0;
+    this.bookTitle = document.getElementById('title');
+    this.bookAuthor = document.getElementById('author');
     }
-
-  addBook () {
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    if(title.trim() === '' || author.trim() === '') {
-      const err = document.createElement('div');
-      err.classList.add('error');
-      err.textContent = "Please enter a title and an author";
-      setTimeout(() => err.remove(), 3500);
-      document.body.appendChild(err);
-     }
-      else if (this.books.some((book) => book.title === title)) {
-        const err = document.createElement('div');
-        err.classList.add('error');
-        err.textContent = "This book already exists";
-        setTimeout(() => err.remove(), 3500);
-        document.body.appendChild(err);
-      } else {
-    const book = {id: ++this.lastId, title, author};
-    this.books.push(book);
-    localStorage.setItem("books", JSON.stringify(this.books));
-    title = '';
-    author = '';
+//Remove book function takes 'book' parameter from 'displayAllBooks' function and removes only that element and update local Storage
+  removeBook(book) {
+    bookList = bookList.filter((element) => element !== book);
+    localStorage.setItem('BooksList', JSON.stringify(bookList));
     this.displayAllBooks();
   }
-}
-
-  removeBook(bookId) {
-    this.books = this.books.filter((book) => book.id !== bookId);
-    localStorage.setItem("books", JSON.stringify(this.books));
-  }
-
+//Clears any previous book list and loops through each book element creating the elements and calling the 'removeBook' function when user clicks the remove btn
   displayAllBooks() {
-    const bookList = document.getElementById('book_list');
-    bookList.innerHTML = "";
-    for (const book of this.books) {
-      const bookListItem = document.createElement("li");
-      bookListItem.innerHTML = `${book.title} <br /> ${book.author}`
-      bookListItem.setAttribute('id', book.id);
-      const line = document.createElement("hr");
-      const btnRemove = document.createElement("button");
-      btnRemove.classList.add('remove-btn')
-      btnRemove.textContent = "Remove";
-      btnRemove.addEventListener('click', () => {
-        this.removeBook(book.id);
-        this.displayAllBooks();
-      });
-      bookListItem.appendChild(btnRemove);
-      bookList.appendChild(bookListItem);
-      bookList.appendChild(line);
-    }
+    allBooks.innerHTML = '';
+    bookList.forEach((book) => {
+    const bookListItem = document.createElement('li');
+    bookListItem.innerHTML = `"${book.bookTitle}" by ${book.bookAuthor}`;
+    const btnRemove = document.createElement('button');
+    btnRemove.innerHTML = 'Remove';
+    btnRemove.className = 'removeButton';
+    btnRemove.addEventListener('click', () => this.removeBook(book));    
+    bookListItem.appendChild(btnRemove);
+    allBooks.appendChild(bookListItem);
+    this.bookTitle.value = '';
+    this.bookAuthor.value = '';
+  });
   };
+// receive the parameters and create an object from them to push it to the global 'bookList' variable and update local Storage then display all books
+  addBook (bookTitle, bookAuthor) {
+    const newBook = { bookTitle, bookAuthor };
+    bookList.push(newBook);
+    localStorage.setItem('BooksList', JSON.stringify(bookList));
+    this.displayAllBooks();
+    this.bookTitle.value = '';
+    this.bookAuthor.value = '';
+}
 }
 
-  const newBook = new Book();
+const newBook = new Book(); 
+newBook.displayAllBooks(); //display all books by default
 
-const addBtn = document.getElementById('submit_btn')
-const formElement = document.getElementById('form');
-
-newBook.displayAllBooks();
-
-addBtn.addEventListener('click', (event) => {
+//Listen when form is busmitted and call addBook function with parameters then display all books if not empty and show error message if it is
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  newBook.addBook();
-  newBook.displayAllBooks();
+  const bookTitle = document.getElementById('title');
+  const bookAuthor = document.getElementById('author');
+  
+  if(bookTitle.value !== '' && bookAuthor.value !== ''){
+    newBook.addBook(bookTitle.value, bookAuthor.value);
+    bookList = JSON.parse(localStorage.getItem('BooksList')) || [];
+    newBook.displayAllBooks();
+    messageField.textContent = '';
+  }else{
+    messageField.textContent = 'Please enter a value'; 
+  };
 });
