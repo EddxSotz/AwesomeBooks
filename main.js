78b4706244339cde95c2d1841e42bdb76980a4b0
Romans Špiļaks <romans.spilaks@gmail.com>
2023-04-26 23:09:29 +0300

    Create basic layout for the app

--- main.js
+++ main.js
@@ -68,3 +68,23 @@ form.addEventListener('submit', (event) => {
     messageField.textContent = 'Please enter a value';
   }
 });
+
+// Date
+
+
+//Change format
+function formatAMPM(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  let strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+
+const dateContainer = document.getElementById('dateContainer');
+let currentDate = new Date();
+dateContainer.innerHTML = currentDate.toLocaleString('en-US');
+
